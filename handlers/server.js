1e68f5f827d656c5836822f14bd5a85bc2e546fe
DmTriet31 <dmtriet.31@gmail.com>
2025-05-09 10:01:31 +0200

    Update server.js

--- handlers/server.js
+++ handlers/server.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
     res.sendFile(imagePath);
 });
 app.listen(port, () => {
-    console.log(`🔗 Listening to GlaceYT : http://localhost:${port}`);
+    console.log(`🔗 Listening to DmTriet : http://localhost:${port}`);
 });
