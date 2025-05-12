a508cc211896bc38644ff7a345894733fb811679
DmTriet31 <dmtriet.31@gmail.com>
2025-05-12 13:20:28 +0200

    Update welcomeMember.js

--- events/welcomeMember.js
+++ events/welcomeMember.js
@@ -9,14 +9,14 @@ module.exports = (client) => {
 
     const embed = new EmbedBuilder()
       .setColor(0xff4757)
-      .setTitle('WELCOME TO VIETZ 🧧')
+      .setTitle('WELCOME TO Radiant Lotus 🧧')
       .setDescription(
         `Chúc bạn có những khoảng khắc vui vẻ và gắn kết với mọi người khi tham gia server, đừng ngần ngại trò chuyện và kết bạn với các thành viên khác nhé.\n\n` +
         `Hãy thoải mái tham gia các cuộc trò chuyện, đóng góp ý tưởng và cùng nhau xây dựng 1 cộng đồng vui vẻ và đoàn kết. ` +
         `Hi vọng bạn có những khoảng khắc tuyệt vời tại server!`
       )
       .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
-      .setFooter({ text: `Viet Zone • ${new Date().toLocaleTimeString()}` });
+      .setFooter({ text: `Radiant Lotus • ${new Date().toLocaleTimeString()}` });
 
     await channel.send({
       content: `🎉 Chào mừng <@${member.id}> đã đến với server, <@&staff> có member mới nè`,
