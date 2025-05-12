9610e343bd25e87b624fa75f68ea7f2d2889b348
DmTriet31 <dmtriet.31@gmail.com>
2025-05-12 12:42:58 +0200

    Create welcomeMember.js

---
+++ events/welcomeMember.js
@@ -0,0 +1,26 @@
+const { Events, EmbedBuilder } = require('discord.js');
+
+module.exports = (client) => {
+  client.on(Events.GuildMemberAdd, async member => {
+    const channel = member.guild.channels.cache.find(
+      ch => ch.name === 'welcome' || ch.id === '1367506963696713891' // sửa lại nếu cần
+    );
+    if (!channel) return;
+
+    const embed = new EmbedBuilder()
+      .setColor(0xff4757)
+      .setTitle('WELCOME TO VIETZ 🧧')
+      .setDescription(
+        `Chúc bạn có những khoảng khắc vui vẻ và gắn kết với mọi người khi tham gia server, đừng ngần ngại trò chuyện và kết bạn với các thành viên khác nhé.\n\n` +
+        `Hãy thoải mái tham gia các cuộc trò chuyện, đóng góp ý tưởng và cùng nhau xây dựng 1 cộng đồng vui vẻ và đoàn kết. ` +
+        `Hi vọng bạn có những khoảng khắc tuyệt vời tại server!`
+      )
+      .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
+      .setFooter({ text: `Viet Zone • ${new Date().toLocaleTimeString()}` });
+
+    await channel.send({
+      content: `🎉 Chào mừng <@${member.id}> đã đến với server, <@&staff> có member mới nè`,
+      embeds: [embed]
+    });
+  });
+};
