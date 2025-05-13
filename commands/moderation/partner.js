d2c3d4f88f20516f2095f727ed1e30a2de0c7364
DmTriet31 <dmtriet.31@gmail.com>
2025-05-13 05:47:44 +0200

    Update partner.js

--- commands/moderation/partner.js
+++ commands/moderation/partner.js
@@ -9,46 +9,49 @@ const {
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('partner')
-    .setDescription('Gửi thông báo partner đẹp như mẫu')
+    .setDescription('Gửi thông báo đối tác mới')
     .addUserOption(option =>
       option.setName('đại_diện')
         .setDescription('Người đại diện cho đối tác')
         .setRequired(true))
     .addStringOption(option =>
-      option.setName('tên_server')
-        .setDescription('Tên server đối tác')
-        .setRequired(true))
-    .addStringOption(option =>
-      option.setName('lĩnh_vực')
-        .setDescription('Lĩnh vực hoạt động')
-        .setRequired(true))
-    .addStringOption(option =>
       option.setName('link')
         .setDescription('Link mời đến server đối tác')
         .setRequired(true))
+    .addStringOption(option =>
+      option.setName('lĩnh_vực')
+        .setDescription('Lĩnh vực hoạt động của đối tác')
+        .setRequired(true)
+        .addChoices(
+          { name: 'Community', value: 'Community' },
+          { name: 'Shop', value: 'Shop' },
+          { name: 'Sell', value: 'Sell' },
+          { name: 'Entertainment', value: 'Entertainment' },
+          { name: 'Gaming', value: 'Gaming' }
+        ))
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageRoles),
 
   async execute(interaction) {
     const user = interaction.options.getUser('đại_diện');
-    const serverName = interaction.options.getString('tên_server');
-    const linhVuc = interaction.options.getString('lĩnh_vực');
     const link = interaction.options.getString('link');
+    const linhVuc = interaction.options.getString('lĩnh_vực');
     const guild = interaction.guild;
 
+    const serverName = user.username; // Tự lấy từ user đại diện
+
     const partnerRoleId = '1367120701869260941';
-    const partnerPostChannelId = '1367120846413631639'; // Đặt lại ID nếu cần
     const outputChannelId = '1367120844878254202'; // Kênh gửi thông báo
 
     const partnerEmbed = new EmbedBuilder()
       .setColor('#FFC0CB')
-      .setDescription(`>>> ✅ **PARTNER**
-<a:ws_verify:1289423707302789177> **Đơn Của Bạn Đã Được Chấp Nhận**
+      .setDescription(`>>> ✅ **PARTNER MỚI**
+<a:RL_verifyanimated:1371693855430283365> **Đối Tác Mới Đã Được Xác Nhận**
 
-<a:ws_partner:1289193885138161664> **Partner**<a:ws_partner:1289193885138161664>
-<a:ws_star_pink:1289417957734223872> **Người Đại Diện:** <@${user.id}>
-<a:ws_papitas:1289417992081506417> **Tên Server:** **${serverName}**
-<:ws_hypesquad_balance:1294648243472437258> **Lĩnh Vực:** **${linhVuc}**
-<a:ws_sparklecolors:1284407469413498961> **Link Server:** ${link}`)
+
+<a:RL_77:1367510222603554827> **Người Đại Diện:** <@${user.id}>
+<a:RL_124:1367510341734371411> **Tên Server:** **${serverName}**
+<a:RL_124:1367510341734371411> **Lĩnh Vực:** **${linhVuc}**
+<a:RL_arrow:1367510296020783184> **Link Server:** ${link}`)
       .setThumbnail(user.displayAvatarURL({ dynamic: true }))
       .setTimestamp()
       .setFooter({
@@ -81,7 +84,7 @@ module.exports = {
 
       // Phản hồi riêng cho người dùng dùng lệnh
       await interaction.reply({
-        content: `✅ Đã đăng partner cho **${serverName}** và cấp role cho ${user.tag}!`,
+        content: `✅ Đã thêm đối tác **${serverName}** và cấp role cho ${user.tag}!`,
         ephemeral: true
       });
 
