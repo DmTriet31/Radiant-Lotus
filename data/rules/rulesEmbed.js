9f44c53772a9a49f8fc458e55a2ad64e4104393e
DmTriet31 <dmtriet.31@gmail.com>
2025-05-14 18:20:49 +0200

    Update rulesEmbed.js

--- data/rules/rulesEmbed.js
+++ data/rules/rulesEmbed.js
@@ -132,6 +132,42 @@ const ruleEmbeds = {
             "<:RL_pinkdot:1371961586608111876> Tôn trọng quyết định của quản trị viên.\n" +
             "<:RL_pinkdot:1371961586608111876> Không tự ý can thiệp khi staff đang xử lý.\n" +
             "<:RL_pinkdot:1371961586608111876> Góp ý nên nhắn riêng cho staff."
+        ),
+
+     Server_Rules : new EmbedBuilder()
+        .setColor("#FF0000")
+        .setTitle("<a:rainbowvivi:1310075817606385724> Luật Chung Cho Toàn Bộ Thành Viên <a:rainbowvivi:1310075817606385724>")
+        .setDescription(
+            "<a:RL_pin:1367510433778372668> Để giữ cho nơi đây luôn vui vẻ - tích cực, chúng mình mong các thành viên tuân thủ một vài điều đơn giản sau:\n\n" +
+
+            "<a:RL_arrow:1371961649690447892> **Tôn trọng lẫn nhau 🤝🏻**\n" +
+            "<:RL_pinkdot:1371961586608111876> Không chửi bới, toxic hay công kích cá nhân. Tuyệt đối không phân biệt chủng tộc, tôn giáo, giới tính...\n\n" +
+
+            "<a:RL_arrow:1371961649690447892> **Không gửi nội dung phản cảm 🔞**\n" +
+            "<:RL_pinkdot:1371961586608111876> Cấm NSFW, bạo lực, nội dung gây khó chịu. Không share link độc hại, giả mạo, scam.\n\n" +
+
+            "<a:RL_arrow:1371961649690447892> **Không spam, quảng cáo 🚫**\n" +
+            "<:RL_pinkdot:1371961586608111876> Hạn chế spam emoji, sticker, ảnh meme liên tục - dù vui nhưng cũng có điểm dừng.\n" +
+            "<:RL_pinkdot:1371961586608111876> Không quảng cáo server khác hoặc dịch vụ bên ngoài (ib cho owner).\n\n" +
+
+            "<a:RL_arrow:1371961649690447892> **Dùng đúng kênh 🗂️**\n" +
+            "<:RL_pinkdot:1371961586608111876> Mỗi kênh đều có mục đích riêng - đăng đúng nội dung đúng chỗ. Lộn xộn là bị quét liền!!!\n\n" +
+
+            "<a:RL_arrow:1371961649690447892> **Không ping staff bừa bãi 🔔**\n" +
+            "<:RL_pinkdot:1371961586608111876> Chỉ ping khi có vấn đề thật sự.\n" +
+            "<:RL_pinkdot:1371961586608111876> Tránh spam tag hoặc cố ý gây phiền phức.\n\n" +
+
+            "<a:RL_pin:1367510433778372668> **Hình phạt nếu vi phạm tuỳ theo mức độ nặng nhẹ, bạn có thể bị:**\n" +
+            "<a:RL_Reddot:1367541939662225409> Nhắc nhở nhẹ qua DM hoặc kênh riêng.\n" +
+            "<a:RL_Reddot:1367541939662225409> Mute (tạm khoá chat 1h), kick, ban.\n" +
+            "<a:RL_Reddot:1367541939662225409> Cấm vĩnh viễn khỏi server nếu tái phạm hoặc vi phạm nghiêm trọng.\n\n" +
+
+            "<a:RL_rainletter:1371820268552589437> **Một chút nhắn nhủ** <a:RL_rainletter:1371820268552589437>\n" +
+            "Server không cần bạn phải hoàn hảo - chỉ cần tôn trọng người khác, vui là chính.\n" +
+            "Tụi mình không cần một cộng đồng quá đông - tụi mình cần một nơi **chất lượng**, nơi mà ai cũng **thoải mái** khi trò chuyện, chia sẻ và luôn **là chính mình 💗**.\n\n" +
+
+            "Cảm ơn vì bạn đã đọc luật! Chúc bạn có khoảng thời gian vui vẻ tại **Radiant Lotus** 🌸\n" +
+            "Nếu có vấn đề gì liên hệ <@853261151365627915> hoặc <#1367120849785716786>"
         )
 };
 
