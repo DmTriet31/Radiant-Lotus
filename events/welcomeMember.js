dad1a4229534b491ae2d25ad5116b576fdbc8ae1
DmTriet31 <dmtriet.31@gmail.com>
2025-05-14 20:05:16 +0200

    Update welcomeMember.js

--- events/welcomeMember.js
+++ events/welcomeMember.js
@@ -9,7 +9,7 @@ module.exports = (client) => {
 
     const embed = new EmbedBuilder()
       .setColor(0xff4757)
-      .setTitle('WELCOME TO Radiant Lotus 🧧')
+      .setTitle('<a:RL_rainbowchloe:1371961708704301207>Welcome To Radiant Lotus <a:RL_rainbowchloe:1371961708704301207>')
       .setDescription(
         `Chúc bạn có những khoảng khắc vui vẻ và gắn kết với mọi người khi tham gia server, đừng ngần ngại trò chuyện và kết bạn với các thành viên khác nhé.\n\n` +
         `Hãy thoải mái tham gia các cuộc trò chuyện, đóng góp ý tưởng và cùng nhau xây dựng 1 cộng đồng vui vẻ và đoàn kết. ` +
