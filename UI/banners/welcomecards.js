c034d497368086f376aa4a09b7e24321322bd188
DmTriet31 <dmtriet.31@gmail.com>
2025-05-23 22:33:53 +0200

    Update welcomecards.js

--- UI/banners/welcomecards.js
+++ UI/banners/welcomecards.js
@@ -3,8 +3,8 @@ const path = require('path');
 const data = {
     welcomeImages: [
         path.resolve(__dirname, '../welcomeimages/1.png'),
-        path.resolve(__dirname, '../welcomeimages/2.jpg'),
-        path.resolve(__dirname, '../welcomeimages/3.jpg'),
+        path.resolve(__dirname, '../welcomeimages/2.gif'),
+        path.resolve(__dirname, '../welcomeimages/3.gif'),
     ],
 };
 
