50669188d06919a8eeb191c098ad7a11b9a8cb9d
DmTriet31 <dmtriet.31@gmail.com>
2025-05-23 22:52:19 +0200

    Update help.js

--- commands/basic/help.js
+++ commands/basic/help.js
@@ -16,9 +16,9 @@ module.exports = {
         
         if (interaction.isCommand && interaction.isCommand()) {
      
-            const BOT_ICON = "https://cdn.discordapp.com/emojis/1334648756649590805.png";
+            const BOT_ICON = "https://cdn.discordapp.com/attachments/1367522678420013146/1367522900445495446/standard_1.gif?ex=6814e472&is=681392f2&hm=5e8a8d444f74a6fea7bdda586a483f2e2a2278e3f55ed4a1c30c92366b0a7570&";
             const EMBED_COLOR = "#5865F2"; 
-            const FOOTER_TEXT = "All In One | The Discord Operating System • Created by GlaceYT";
+            const FOOTER_TEXT = "Radiant Lotus • DmTriet | The Discord Operating System";
             const COMMANDS_DIR = path.join(__dirname, '../../commands');
             const EXCESS_COMMANDS_DIR = path.join(__dirname, '../../excesscommands');
 
@@ -117,7 +117,7 @@ module.exports = {
 
                 
                 pages.push({
-                    title: '✨ ALL IN ONE BOT',
+                    title: '✨ Radiant Lotus',
                     description: [
                         '### THE DISCORD OPERATING SYSTEM',
                         '',
@@ -132,7 +132,7 @@ module.exports = {
                         `\`💻\` **Prefix Commands:** ${Object.values(config.excessCommands).some(v => v) ? '`Enabled`' : '`Disabled`'}`,
                         '',
                     ].join('\n'),
-                    author: { name: 'ALL IN ONE BOT • COMMAND CENTER' },
+                    author: { name: 'Radiant Lotus • COMMAND CENTER' },
                     icon: '📚'
                 });
 
@@ -202,7 +202,7 @@ module.exports = {
                     .setAuthor({
                         name: page.author.name,
                         iconURL: BOT_ICON,
-                        url: "https://discord.gg/xQF9f9yUEM"
+                        url: "https://discord.gg/ssh2hgpadH"
                     })
                     .setImage(helpBanner)
                     .setFooter({ text: `${FOOTER_TEXT} • Page ${currentPage + 1}/${currentSet.length}` })
@@ -378,10 +378,10 @@ module.exports = {
                 .setAuthor({
                     name: "Command Error",
                     iconURL: cmdIcons.dotIcon,
-                    url: "https://discord.gg/xQF9f9yUEM"
+                    url: "https://discord.gg/ssh2hgpadH"
                 })
                 .setDescription('> ⚠️ This command can only be used as a slash command!\n> Please use `/help` instead.')
-                .setFooter({ text: 'All In One Bot • Error' })
+                .setFooter({ text: 'Radiant Lotust • Error' })
                 .setTimestamp();
 
           
