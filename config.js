34e7d1dd1a1e294bdf90dce61994b56e83113036
DmTriet31 <dmtriet.31@gmail.com>
2025-05-24 21:49:02 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ module.exports = {
     rotateDefault: [
       { name: 'Netflix với Tnhi', type: ActivityType.Watching },
       { name: 'Bê đê', type: ActivityType.Playing },
-      { name: 'Restream hôm qua', type: ActivityType.Streaming, url: 'https://www.youtube.com/@MixiGaming3con' },
+      { name: 'Netflix', type: ActivityType.Streaming, url: 'https://www.youtube.com/@MixiGaming3con' },
       { name: 'Anh DmTriet chửi', type: ActivityType.Listening },
     ],
     songStatus: true
