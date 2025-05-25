8999000ded172ab4da7c4d1555f2e3e0867e315e
DmTriet31 <dmtriet.31@gmail.com>
2025-05-25 22:22:04 +0200

    Update welcomeMember.js

--- events/welcomeMember.js
+++ events/welcomeMember.js
@@ -3,13 +3,13 @@ const { Events, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = r
 module.exports = (client) => {
   client.on(Events.GuildMemberAdd, async member => {
     const channel = member.guild.channels.cache.find(
-      ch => ch.name === 'welcome' || ch.id === '1367506963696713891'
+      ch => ch.name === 'welcome' || ch.id === '1376211389324202177'
     );
     if (!channel) return;
 
     const embed = new EmbedBuilder()
       .setColor(0xff4757)
-      .setTitle('<a:RL_rainbowchloe:1371961708704301207> Welcome To Radiant Lotus <a:RL_rainbowchloe:1371961708704301207>')
+      .setTitle('<a:RL_rainbowchloe:1376245313853063250> Welcome To Radiant Lotus <a:RL_rainbowchloe:1376245313853063250>')
       .setDescription(
         `Chúc bạn có những khoảng khắc vui vẻ và gắn kết với mọi người khi tham gia server, đừng ngần ngại trò chuyện và kết bạn với các thành viên khác nhé.\n\n` +
         `Hãy thoải mái tham gia các cuộc trò chuyện, đóng góp ý tưởng và cùng nhau xây dựng 1 cộng đồng vui vẻ và đoàn kết. ` +
@@ -23,10 +23,10 @@ module.exports = (client) => {
 
     const row = new ActionRowBuilder().addComponents(
       new ButtonBuilder()
-        .setLabel('.gg/ssh2hgpadH')
+        .setLabel('.gg/em2xksJvZX')
         .setStyle(ButtonStyle.Link)
-        .setURL('https://discord.com/channels/1367120428648108042/1367120774300700763') // Link mời của server
-        .setEmoji('<a:62802:1210522480901496863>'),
+        .setURL('https://discord.com/channels/1369830175700942959/1376211346294833152') // Link mời của server
+        .setEmoji('<a:RL_62802:1376215865036636182>'),
     );
 
     await channel.send({
