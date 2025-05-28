5063830f80bd8a33893f5e33e4ff47539198f295
DmTriet31 <dmtriet.31@gmail.com>
2025-05-28 18:37:26 +0200

    Update memberLeaveHandler.js

--- logHandlers/memberLeaveHandler.js
+++ logHandlers/memberLeaveHandler.js
@@ -35,7 +35,7 @@ module.exports = async function memberLeaveHandler(client) {
             const channel = member.guild.channels.cache.get(leaveSettings.leaveChannelId);
             if (channel) {
                 const embed = new EmbedBuilder()
-                    .setTitle('୨ <:RL_Lounge_bow:1373276638074245190> ୧〃 ➜ *New Leave*')
+                    .setTitle('୨ <:RL_Lounge_bow:1376217725994663957> ୧〃 ➜ *New Leave*')
                     .setColor('#FF9900')
                     .setDescription(`Con Quỷ ${member.user.tag} đã bỏ rơi chúng ta.`)
                     .setThumbnail(member.user.displayAvatarURL())
