cca37ebcb928fe204eee8922e098ffed1861dd77
DmTriet31 <dmtriet.31@gmail.com>
2025-06-07 14:29:50 +0200

    Update SetupBanners.js

--- UI/banners/SetupBanners.js
+++ UI/banners/SetupBanners.js
@@ -1,5 +1,5 @@
 const setupBanners = {
-    helpBanner : 'https://cdn.discordapp.com/attachments/1367522678420013146/1367522834787729548/standard.gif?ex=6814e463&is=681392e3&hm=851175dde7a8e999bfe5a37f0eda1ef9934b76b49e267264c1df6ce6b7d83be6&',
+    helpBanner : 'https://cdn.discordapp.com/attachments/1376211384626446411/1380884482944667702/Radiant_Lotus_-_Animated_2.gif?ex=68458064&is=68442ee4&hm=ad6d83105a52fea3b215aa98db24c897c36be9dd380d2d9597f092406659b854&',
     verificationBanner : 'https://cdn.discordapp.com/attachments/1367522678420013146/1367522834787729548/standard.gif?ex=6814e463&is=681392e3&hm=851175dde7a8e999bfe5a37f0eda1ef9934b76b49e267264c1df6ce6b7d83be6&',
     ticketBanner : 'https://i.ibb.co/h1DBsRnv/ticket.gif',
     autovcBanner : 'https://i.ibb.co/mk6Tj1r/autovc.gif',
