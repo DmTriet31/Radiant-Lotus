f0f5da35bdf20eebf67f8dd624a5137d7ddfdeb2
DmTriet31 <dmtriet.31@gmail.com>
2025-06-12 14:48:06 +0200

    Update server.js

--- handlers/server.js
+++ handlers/server.js
@@ -1,14 +1,11 @@
 const path = require('path');
 const express = require("express");
 const app = express();
-
-const port = process.env.PORT || 3000; // SỬA CHỖ NÀY
-
+const port = 3000;
 app.get('/', (req, res) => {
     const imagePath = path.join(__dirname, 'index.html');
     res.sendFile(imagePath);
 });
-
 app.listen(port, () => {
-    console.log(`🔗 Listening on http://localhost:${port}`);
+    console.log(`🔗 Listening to: http://localhost:${port}`);
 });
